8d11a12b48eb1375645da7fff37027a81641b9d4
Assisstant245 <ayeshasiddique9241>
2026-02-06 12:49:22 +0500

    ...

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
     ],
-    build: {
-        outDir: 'dist', 
-        emptyOutDir: true, 
-    },
+    
 });
